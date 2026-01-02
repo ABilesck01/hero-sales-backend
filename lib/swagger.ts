5fc2d0a22487ed9ec5fa6bf31ac588e69bb08620
Alex Pontes <alex.bileck@gmail.com>
2026-01-02 20:56:57 -0300

    Update lib/swagger

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -1,14 +1,23 @@
+import type { OpenAPIV3 } from "openapi-types";
+
 const enabled = process.env.ENABLE_SWAGGER === "true";
 
-export const swaggerSpec = enabled
-  ? swaggerJSDoc({
-      definition: {
-        openapi: "3.0.0",
-        info: {
-          title: "HERO Sales API",
-          version: "1.0.0",
+export const swaggerSpec: OpenAPIV3.Document | null = enabled
+  ? (() => {
+      // 👇 IMPORT DINÂMICO (só executa se enabled === true)
+      // evita erro de build na Vercel
+      // evita precisar do pacote em produção
+      const swaggerJSDoc = require("swagger-jsdoc");
+
+      return swaggerJSDoc({
+        definition: {
+          openapi: "3.0.0",
+          info: {
+            title: "HERO Sales API",
+            version: "1.0.0",
+          },
         },
-      },
-      apis: ["./app/api/**/*.ts"],
-    })
+        apis: ["./app/api/**/*.ts"],
+      });
+    })()
   : null;
